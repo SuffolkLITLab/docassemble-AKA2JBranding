ad575763e86fc8ffd276e65f08617ef75eccb859
Quinten Steenhuis <qsteenhuis@suffolk.edu>
2025-08-26 17:29:53 -0400

    Only import log if needed

--- docassemble/AKA2JBranding/advertise_weaver.py
+++ docassemble/AKA2JBranding/advertise_weaver.py
@@ -1,11 +1,10 @@
 # pre-load
 import os
-from docassemble.base.util import log
-
 try:
     from docassemble.ALWeaver.custom_values import advertise_capabilities
     if not os.environ.get('ISUNITTEST'):
         advertise_capabilities(__name__, minimum_version="1.5")
 
 except ImportError:
+    from docassemble.base.util import log
     log("Not advertising capabilities because ALWeaver is not installed.")
