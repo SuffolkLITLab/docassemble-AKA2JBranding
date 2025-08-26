086245e66d2f6236c4d13b7db864a32324800bb6
Quinten Steenhuis <qsteenhuis@suffolk.edu>
2025-08-26 17:29:09 -0400

    Reduce annoying log messages when Weaver isn't installed

--- docassemble/AKA2JBranding/advertise_weaver.py
+++ docassemble/AKA2JBranding/advertise_weaver.py
@@ -1,7 +1,11 @@
 # pre-load
 import os
-from docassemble.ALWeaver.custom_values import advertise_capabilities
+from docassemble.base.util import log
 
-if not os.environ.get('ISUNITTEST'):
-    advertise_capabilities(__name__, minimum_version="1.5")
+try:
+    from docassemble.ALWeaver.custom_values import advertise_capabilities
+    if not os.environ.get('ISUNITTEST'):
+        advertise_capabilities(__name__, minimum_version="1.5")
 
+except ImportError:
+    log("Not advertising capabilities because ALWeaver is not installed.")
